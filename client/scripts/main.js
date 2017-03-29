95d8092c529249486ba6aa3d599898f747a67a92
Joanna Kao <joannaskao@gmail.com>
2017-03-29 06:40:17 -0400

    add case for adding 'not yet' response

--- client/scripts/main.js
+++ client/scripts/main.js
@@ -2,6 +2,9 @@ fetch('https://bertha.ig.ft.com/view/publish/gss/1df2KhuBMRYJji9SDX528c8RGGGaMPA
   .then(function(res) {
     res.json().then(function(jsonresponse) {
       if (jsonresponse[0].result) {
+        if (jsonresponse[0].result === 'not yet') {
+          document.getElementById('answer').innerHTML = 'Not yet';
+        }
         document.getElementById('answer').innerHTML = 'Yes';
       } else {
         document.getElementById('answer').innerHTML = 'No';
