e03fca9b5a158c7f5d615fa41bd86db8aa705a1f
Joanna Kao <joannaskao@gmail.com>
2017-03-29 07:35:40 -0400

    fix logic

--- client/scripts/main.js
+++ client/scripts/main.js
@@ -1,13 +1,16 @@
 fetch('https://bertha.ig.ft.com/view/publish/gss/1df2KhuBMRYJji9SDX528c8RGGGaMPASDyCmFB7PZRSE/options')
   .then(function(res) {
     res.json().then(function(jsonresponse) {
-      if (jsonresponse[0].result) {
-        if (jsonresponse[0].result === 'not yet') {
-          document.getElementById('answer').innerHTML = 'Not yet';
-        }
-        document.getElementById('answer').innerHTML = 'Yes';
-      } else {
+      if (jsonresponse[0].result === 'no') {
         document.getElementById('answer').innerHTML = 'No';
+      }
+
+      if (jsonresponse[0].result === 'yes') {
+        document.getElementById('answer').innerHTML = 'Yes';
+      }
+
+      if (jsonresponse[0].result === 'not yet') {
+        document.getElementById('answer').innerHTML = 'Not yet';
       }
 
       const todayDate = moment().format('D MMM YYYY');
